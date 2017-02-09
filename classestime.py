0fad02e213695985eeef15c27d55e8d54ffd6445
lokeshbalcha <lokeshbalcha@gmail.com>
2017-02-09 14:55:42 -0800

    classes time

---
+++ classestime.py
@@ -0,0 +1,20 @@
+#lets make a calculator
+
+class calcu:
+    def add(x,y):
+        ans = x+y
+        print(ans)
+    def sub(x,y):
+        ans = x-y
+        print(ans)
+    def mult(x,y):
+        ans = x*y
+        print(ans)
+    def div(x,y):
+        ans = x/y
+        print(ans)
+
+
+calcu.add(2,3)
+calcu.div(3,6)
+calcu.mult(41,3)
