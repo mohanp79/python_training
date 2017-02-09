2e7b8a8bda875c1e91d7f4c48803013e3f0f6415
lokeshbalcha <lokeshbalcha@gmail.com>
2017-02-09 14:43:32 -0800

    adding appending file

--- writing_file.py
+++ writing_file.py
@@ -4,3 +4,10 @@ write_file = open('example_file.txt','w')
 
 write_file.write(write_content)
 write_file.close()
+
+appendFile = 'appending text to existing file'
+
+saveFile = open('example_file.txt','a')
+saveFile.write('\n')
+saveFile.write(appendFile)
+saveFile.close()
